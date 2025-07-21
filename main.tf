95ac71cfdf2dfa46427081f1f5c9fa715c46d339
sharmah91 <sharmah91@gmail.com>
2025-07-21 14:50:47 +0530

    Terraform_Resource_Group

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f96f2e3b-2cc8-4e5e-8ed9-4afb664bb5a8"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "hiteshrg"
+  location = "East US"
+}
