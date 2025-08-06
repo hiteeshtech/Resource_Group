a1ac79d08a2b83883aa4a8aa49f20992e9af4e69
sharmah91 <sharmah91@gmail.com>
2025-08-06 13:30:28 +0530

    added backend block because of pipeline

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>4.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "pooranrg"
+    storage_account     = "stgpipe123"
+    container_name      = "pipecontainer"
+    key                 = "terraform.tfstate"
+  }
+
 }
 
 provider "azurerm" {
